f1af39e16887d5c15c7cbcce8d9889a45fb0c9e0
logdan <logandanek@gmail.com>
2023-12-06 13:42:01 -0500

    update install packages

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+# Install shell, p10k, basic packages
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+sudo apt-get install zsh -y
+chsh -s $(which zsh)
+
+git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git -C ~/.oh-my-zsh/custom/plugins clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git
+git -C ~/.oh-my-zsh/custom/plugins clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git
+git -C ~/.oh-my-zsh/custom/themes clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+
+# Note that chezmoi already manages the configuration of .zshrc and .p10k.zsh in the $HOME dir
+
 # Install packages for shell runs
 
 sudo apt install ripgrep
