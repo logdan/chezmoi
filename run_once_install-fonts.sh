ff12145b877a0dbae45af272150d407d0e664e61
logdan <logandanek@gmail.com>
2023-12-06 14:43:40 -0500

    make downloads

--- run_once_install-fonts.sh
+++ run_once_install-fonts.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Download and install fonts
+mkdir -p $HOME/Downloads
 git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts $HOME/Downloads/nerd-fonts
 cd $HOME/Downloads/nerd-fonts
 git sparse-checkout add patched-fonts/JetBrainsMono
